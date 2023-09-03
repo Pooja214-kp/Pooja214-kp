7689767de53e88f31ec1436c47455a4cfb08d9d9
pooja214-kp <pooja.kumarjb@gmail.com>
2023-09-03 14:26:04 +0530

    data4

--- data/dataset.py
+++ data/dataset.py
@@ -118,8 +118,10 @@ class Data(Dataset):
 
         if image is None or mask is None:
         # Handle the case where image or mask loading failed
-        # You can choose to skip this sample or handle the error as needed
-            return None, None, None, None
+        # Replace the failed sample with placeholder data
+            image = np.zeros((H, W, C), dtype=np.float32)
+            mask = np.zeros((H, W, C), dtype=np.float32)
+
         
         imagepath, maskpath = self.samples[idx]
         image               = cv2.imread(imagepath).astype(np.float32)[:,:,::-1]
